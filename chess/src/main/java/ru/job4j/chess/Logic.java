95b81874e6ebf1723b790eab907ea00146d36e39
ubatyr <ubatyr@mail.ru>
2022-05-03 12:36:43 +0500

    2. Каркас шахматной доски [#792 #295166]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -21,9 +21,9 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
-        for (int index = 0; index < figures.length; index++) {
-            for (int i = 0; i < steps.length; i++) {
-                if (figures[index] != null && figures[index].position().equals(steps[i])) {
+        for (Figure figure : figures) {
+            for (Cell cell : steps) {
+                if (figure != null && figure.position().equals(cell)) {
                     throw new OccupiedCellException();
                 }
             }
